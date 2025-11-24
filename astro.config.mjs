da778bf5642d7f78e717cbd00df096801bb59428
destroyer <destroyer.dcf@gmail.com>
2025-11-24 19:04:13 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://cpcready.github.io',
+	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'Documentation Site',
