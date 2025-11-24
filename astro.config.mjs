f91a302545ed2156630601455dc03389aa9e398f
destroyer <destroyer.dcf@gmail.com>
2025-11-24 19:36:48 +0100

    docs: arreglamos errores en la navegacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cpcready.github.io',
-	base: '/docs',
+	base: process.env.NODE_ENV === 'production' ? '/docs' : '/',
 	integrations: [
 		starlight({
 			title: 'Documentation Site',
